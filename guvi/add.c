28219b81bb1a7dc0c93f65697a23ad9e416d4c40
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-01-05 16:27:07 +0530

    Create add.c

---
+++ guvi/add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a<0)
+printf("negative");
+else if(a>0)
+printf("positive");
+else
+printf("zero");
+}
