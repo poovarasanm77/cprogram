f2a32910157d11c5d1238b60e1fa7476a0869f7d
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-01-08 16:40:56 +0530

    Create VOWLES.C

---
+++ GUVI/VOWLES.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char n;
+printf("enter the letter");
+scanf("%c",&n);
+if(n==a||n==e||n==i||n==o||n==u)
+{
+printf("VOWELS");
+}
+else
+{
+printf("consonants");
+}
+}
