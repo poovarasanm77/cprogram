9978c95ed744174654e8e65a0f2b598d9cbb31e2
RJANANIESWARI <35128124+RJANANIESWARI@users.noreply.github.com>
2018-01-10 16:57:14 +0530

    Create new file

---
+++ array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int k,i,n,a[i],sum=0;
+printf("enter the numbers");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("enter the k values");
+scanf("%d",&k);
+if(i<=k)
+{
+sum=sum+a[i];
+}
+printf("sum is %d",sum);
+}
